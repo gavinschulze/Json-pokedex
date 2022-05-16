903555d8e21fb7ba01e28f61ff1c0f5f00c77424
gavinschulze <71295796+gavinschulze@users.noreply.github.com>
2022-05-16 08:23:16 -0700

    Add files via upload

---
+++ pokemon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace json_pokedex
+{
+    class Pokemon
+    {
+        public string hp { get; set; }
+
+        public string pName { get; set; }
+      
+        public string move { get; set; }
+
+        public string type { get; set; }
+
+        public string picture { get; set; }
+
+    }
+}
